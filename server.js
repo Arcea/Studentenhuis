ed767205a45859402b1404d6ae3b3f03501f8418
Arcea <rowinvanblokland@hotmail.nl>
2018-01-16 00:59:06 +0100

    These should not be commented

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ let routes = require('./routes');
 const app = express();
 
 app.use(bodyParser.urlencoded({ 'extended': 'false' }));
-// app.use(bodyParser.json());
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(routes);
 
 let port = process.argv[2] || process.env.PORT || 8000;
