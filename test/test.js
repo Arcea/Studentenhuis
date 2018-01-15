2af99d57c0f569abe42cf6b2f993c498db460242
Wouter <wouter97@planet.nl>
2018-01-15 21:28:36 +0100

    Added database, first test

--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-//var index = require('../index.js');
+var routes = require('../server.js');
 var chould = chai.should();
 
 chai.use(chaiHttp);
-/* Routes moet nog gemaakt worden.
+
 //Een basis test, checkt of / te bereiken is.
 //Faalt deze test controleer dan of de app wel aan staat
 describe('Test of de app correct draait', function(){
-    it('GET /api/', function(done) {
-        chai.request(index)
-            .get('/api/')
+    it('GET /', function(done) {
+        chai.request(routes)
+            .get('/')
             .end(function (err, res){
                 res.should.have.status(200);
                 res.body.should.be.a('object');
@@ -18,4 +18,3 @@ describe('Test of de app correct draait', function(){
         })
     })
 });
-*/
