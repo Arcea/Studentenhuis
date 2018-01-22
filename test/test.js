4423b0a9c147631686ad2c4807765395e6c98497
Wouter <wouter97@planet.nl>
2018-01-22 14:45:42 +0100

    Added first set of tests

--- test/test.js
+++ test/test.js
@@ -5,16 +5,45 @@ var chould = chai.should();
 
 chai.use(chaiHttp);
 
-//Een basis test, checkt of / te bereiken is.
-//Faalt deze test controleer dan of de app wel aan staat
+/* Testcases Wouter 
+Een basis test, checkt of / te bereiken is.
+Faalt deze test controleer dan of de app wel aan staat. 
+Register moet altijd bereikbaar zijn voor de app. Geen JWT nodig.
+*/
 describe('Test of de app correct draait', function(){
     it('GET /', function(done) {
         chai.request(routes)
-            .get('/')
-            .end(function (err, res){
+        .get('/')
+        .end(function (err, res){
                 res.should.have.status(200);
                 res.body.should.be.a('object');
-                done();
+                //res.body.should.contain('Welcome');
+            done();
         })
     })
 });
+
+/*
+Het registreren van een user, dit moet gedaan worden om toegang te krijgen.
+register.post is nog beschikbaar zonder JWT. Alle functionaliteiten hierna hebben authenticatie nodig.
+*/
+describe('Het registreren van een gebruiker', function(){
+    it('POST /register', function(done){
+        var testUser = {
+            name: "tester",
+            email: "test@mail.nl",
+            //pwd krijgt een hash door de server
+            password: "123"
+        }
+        
+        chai.request(routes)
+        .post('/register')
+        .send(testUser)
+        .end(function (err, res){
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property("status").eql("success");
+            done();
+        })
+    })
+})
