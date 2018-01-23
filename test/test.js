2c5da270a7c6ee13152ba472668b518a51604766
JRoffel <jasonroffel@hotmail.nl>
2018-01-23 11:58:08 +0100

    Test cases

--- test/test.js
+++ test/test.js
@@ -47,3 +47,97 @@ describe('Het registreren van een gebruiker', function(){
         })
     })
 })
+
+// Testcases Jason
+// Toevoegen van maaltijden
+describe('Toevoegen van een maaltijd', function(){
+    let loginToken = null;
+    before(function(done){
+        var testUser = {
+            name: "tester",
+            email: "test@mail.nl",
+            password: "123"
+        }
+        chai.request(routes)
+            .post('/login')
+            .send(testUser)
+            .end(function (err, res){
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property("status").eql("success");
+                    res.body.should.have.property("token");
+                    loginToken = res.body.token;
+                    console.log(loginToken);
+                done();
+            })  
+    });
+    it('POST /maaltijd', function(done){
+        var testMeal = {
+            idKok: 2,
+            naamMaaltijd: 'Test maaltijd',
+            maaltijdAfbeelding: 'null',
+            maxEters: '200',
+            maaltijdBeginTijd: '2018-01-22 16:30:00.000000',
+            kosten: '20',
+            beschrijving: 'Test maaltijd'
+        }
+        chai.request(routes)
+            .post('/maaltijd')
+            .set('authentication', loginToken)
+            .send(testMeal)
+            .end(function (err, res){
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              console.log(res.body);
+              res.body.should.have.property("status").eql("OK");
+            done();
+        })
+    })
+});
+// Updaten van maaltijden
+describe('Updaten van een maaltijd', function(){
+    let loginToken = null;
+    before(function(done){
+        var testUser = {
+            name: "tester",
+            email: "test@mail.nl",
+            password: "123"
+        }
+        chai.request(routes)
+            .post('/login')
+            .send(testUser)
+            .end(function (err, res){
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property("status").eql("success");
+                    res.body.should.have.property("token");
+                    loginToken = res.body.token;
+                    console.log(loginToken);
+                done();
+            })  
+    });
+    it('PUT /maaltijd/:id', function(done){
+        var id = 2;
+        var updatedMeal = {
+            idKok: 2,
+            naamMaaltijd: 'Test maaltijd',
+            maaltijdAfbeelding: 'null',
+            maxEters: '200',
+            maaltijdBeginTijd: '2018-01-22 16:30:00.000000',
+            kosten: '20',
+            beschrijving: 'Test maaltijd'
+        }
+        chai.request(routes)
+            .put('/maaltijd/' +id)
+            .set('authentication', loginToken)
+            .send(updatedMeal)
+            .end(function (err, res){
+              console.log(res.body);
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.should.have.property("status").eql("OK");
+            done();
+        })
+    })
+});
+// End Jason testcases
