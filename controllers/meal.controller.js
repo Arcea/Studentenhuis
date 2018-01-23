0affbdd99ac839122582c12453d3a4ebcbc62e30
JRoffel <jasonroffel@hotmail.nl>
2018-01-23 12:09:46 +0100

    Images upload

--- controllers/meal.controller.js
+++ controllers/meal.controller.js
@@ -1,5 +1,6 @@
 var meals = require('../models/meal');
 var encrypt = require('../Helper/encrypt');
+let fs = require("fs");
 
 module.exports = {
     getMeals(req, res, next) {
@@ -46,7 +47,31 @@ module.exports = {
         if (!req.body.idKok || !req.body.naamMaaltijd || !req.body.maxEters || !req.body.maaltijdBeginTijd || !req.body.kosten) {
             res.sendStatus(400);
         } else {
-            meals.addMeal([req.body.idKok, req.body.naamMaaltijd, req.body.maaltijdAfbeelding, req.body.maxEters, req.body.maaltijdBeginTijd, req.body.kosten, req.body.beschrijving], function (err, result) {
+            let maaltijdAfbeeldingPath = null;
+            if(req.body.maaltijdAfbeelding) {
+                maaltijdAfbeelingPath = "../../images/" + req.body.naamMaaltijd + req.body.idKok + req.body.kosten + req.body.maaltijdBeginTijd;
+
+                if(req.body.maaltijdAfbeelding.contains("/^data:image\/png;base64,/")) {
+                    maaltijdAfbeeldingPath += ".png";
+
+                    let base64Image = req.body.maaltijdAfbeelding.replace(/^data:image\/png;base64,/, "");
+
+                    fs.writeFile(maaltijdAfbeeldingPath, base64Image, 'base64', function(err) {
+                        console.log(err);
+                    });
+                } else if (req.body.maaltijdAfbeelding.contains("/^data:image\/jpeg;base64,/")) {
+                    maaltijdAfbeeldingPath += ".jpg";
+
+                    let base64Image = req.body.maaltijdAfbeelding.replace(/^data:image\/jpeg;base64,/, "");
+
+                    fs.writeFile(maaltijdAfbeeldingPath, base64Image, 'base64', function(err) {
+                        console.log(err);
+                    });
+                } else {
+                    res.status(400).end("Images only support .png and .jpg");
+                }
+            }
+            meals.addMeal([req.body.idKok, req.body.naamMaaltijd, maaltijdAfbeeldingPath, req.body.maxEters, req.body.maaltijdBeginTijd, req.body.kosten, req.body.beschrijving], function (err, result) {
                 if (err) {
                     next(err);
                 } else {
